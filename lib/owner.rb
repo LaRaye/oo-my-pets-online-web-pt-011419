3ae00d89b7b042c49c8684c66280c52558796ae3
LaRaye Johnson <lnj2105@gmail.com>
2019-02-15 02:49:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,7 +51,7 @@ class Owner
     @pets[:cats].each {|cat| cat.mood = "happy"}
   end 
   
-  def feed_the_fishes
-    @pets[:fishes].each {|fish| fish.mood = "happy"}
+  def feed_fish
+    self.pets[:fishes].each {|fish| fish.mood = "happy"}
   end 
 end
